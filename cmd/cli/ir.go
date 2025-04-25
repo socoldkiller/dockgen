1332ecca4fc9f0b031dbdf0d460ea2f787e98d1e
socoldkiller <2119306250@qq.com>
2025-04-25 15:10:39 +0800

    feat: add ir output

--- cmd/cli/ir.go
+++ cmd/cli/ir.go
@@ -4,10 +4,47 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 package cli
 
 import (
-	"fmt"
-
+	"dockgen/pkg/analysis"
+	"dockgen/pkg/analysis/types"
+	"dockgen/pkg/command"
+	"dockgen/pkg/util"
+	"encoding/json"
 	"github.com/spf13/cobra"
+	"os"
 )
+
+type ExportIR struct {
+	Cmd         string            `json:"cmd,omitempty"`
+	Program     string            `json:"program,omitempty"`
+	PipeCommand []*ExportIR       `json:"pipeCommand,omitempty"`
+	Options     map[string]string `json:"options,omitempty"`
+	Args        []string          `json:"args,omitempty"`
+	Input       *string           `json:"input,omitempty"`
+	Output      *string           `json:"output,omitempty"`
+	Redirect    *string           `json:"redirect,omitempty"`
+	Env         *types.Env        `json:"env,omitempty"`
+}
+
+func NewExportIR(ir types.IR) *ExportIR {
+	var pipeExportIR []*ExportIR
+	for _, pipe := range ir.PipeCommand() {
+		ir := NewExportIR(pipe)
+		pipeExportIR = append(pipeExportIR, ir)
+	}
+
+	return &ExportIR{
+		Cmd:         ir.Cmd(),
+		Program:     ir.Program(),
+		Options:     ir.Options(),
+		Args:        ir.Args(),
+		Input:       ir.Input(),
+		Output:      ir.Output(),
+		Redirect:    ir.Redirect(),
+		Env:         ir.Env(),
+		PipeCommand: pipeExportIR,
+	}
+
+}
 
 // irCmd represents the ir command
 var irCmd = &cobra.Command{
@@ -16,8 +53,36 @@ var irCmd = &cobra.Command{
 	Long: `The 'ir' command parses recorded shell commands into an Intermediate Representation (IR).
 This IR serves as the foundation for later analysis, graph construction, and Dockerfile generation.
 It supports input from JSON files or directly from standard input.`,
+	Args: cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("ir called")
+
+		var IRList []*ExportIR
+		for _, arg := range args {
+			f, err := os.Open(arg)
+
+			if err != nil {
+				continue
+			}
+
+			var result []*command.Result
+			err = json.NewDecoder(f).Decode(&result)
+			if err != nil {
+				continue
+			}
+
+			builder := analysis.NewBashIRBuilder()
+			cmdIRs, err := builder.Build(result)
+			if err != nil {
+				continue
+			}
+
+			for _, ir := range cmdIRs {
+				export := NewExportIR(ir)
+				IRList = append(IRList, export)
+			}
+		}
+
+		util.SuccessOutput(IRList, "", "json")
 	},
 }
 
---
+++ pkg/util/output.go
@@ -0,0 +1,44 @@
+package util
+
+import (
+	"dockgen/pkg/log"
+	"fmt"
+	"gopkg.in/yaml.v3"
+	"os"
+)
+
+func output(result interface{}, override string, outputFormat string) string {
+	var jsonData string
+	switch outputFormat {
+	case "json":
+		jsonData = JSONf(result)
+	case "json-line":
+		jsonData = JSONf(result)
+	case "yaml":
+		jsonBytes, err := yaml.Marshal(result)
+		if err != nil {
+			log.Fatalf("failed to unmarshal output")
+		}
+		jsonData = string(jsonBytes)
+	default:
+		return override
+	}
+
+	return jsonData
+}
+
+// SuccessOutput prints the result to stdout and exits with status code 0.
+func SuccessOutput(result interface{}, override string, outputFormat string) {
+	fmt.Println(output(result, override, outputFormat))
+	os.Exit(0)
+}
+
+// ErrorOutput prints an error message to stderr and exits with status code 1.
+func ErrorOutput(errResult error, override string, outputFormat string) {
+	type errOutput struct {
+		Error string `json:"error"`
+	}
+
+	fmt.Fprintf(os.Stderr, "%s\n", output(errOutput{errResult.Error()}, override, outputFormat))
+	os.Exit(1)
+}
