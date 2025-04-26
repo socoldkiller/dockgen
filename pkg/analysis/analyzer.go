8e811109ff5e65a2607318a628a41d03e9f859bd
socoldkiller <2119306250@qq.com>
2025-04-27 00:35:07 +0800

    feat: add system env and user env

--- pkg/analysis/analyzer.go
+++ pkg/analysis/analyzer.go
@@ -14,28 +14,29 @@ type Analyzer interface {
 }
 
 type EnvAnalyzer struct {
-	envTable map[string]string
+	userTable   map[string]string
+	systemTable map[string]string
 }
 
 func NewEnvAnalyzer() *EnvAnalyzer {
 	return &EnvAnalyzer{
-		envTable: make(map[string]string),
+		userTable: make(map[string]string),
 	}
 }
 
-func (e *EnvAnalyzer) Analyze(graph types.IRGraph) error {
+func analyzeSystemEnv(graph types.IRGraph) (map[string]string, error) {
 	g, ok := graph.(*types.BashGraph)
 	if !ok {
-		return fmt.Errorf("invalid graph type")
+		return nil, fmt.Errorf("invalid graph type")
 	}
-	exportNodes := g.GetCmdGroup("env", "family")
+	systemNodes := g.GetCmdGroup("env", "family")
 
 	var envNode types.IR
-	if len(exportNodes) == 0 {
-		return nil
+	if len(systemNodes) == 0 {
+		return nil, fmt.Errorf("not found system command")
 	}
 
-	envNode = exportNodes[len(exportNodes)-1]
+	envNode = systemNodes[len(systemNodes)-1]
 	stdout := envNode.Stdout()
 	lines := strings.Split(stdout, "\n")
 
@@ -46,21 +47,55 @@ func (e *EnvAnalyzer) Analyze(graph types.IRGraph) error {
 	})
 	envBuilder := NewBashIRBuilder()
 	envList, err := envBuilder.Build(res)
+
+	if err != nil {
+		return nil, err
+	}
+
+	systemEnv := make(map[string]string)
+	for _, env := range envList {
+		systemEnv[env.Env().EnvVariable] = env.Env().EnvValue.Value
+	}
+
+	return systemEnv, nil
+}
+
+func (e *EnvAnalyzer) Analyze(graph types.IRGraph) error {
+	systemEnv, err := analyzeSystemEnv(graph)
 	if err != nil {
 		return err
 	}
+	e.systemTable = systemEnv
+	g := graph.(*types.BashGraph)
 
-	for _, env := range envList {
-		e.envTable[env.Env().EnvVariable] = env.Env().EnvValue.Value
+	exportNodes := g.GetCmdGroup("export", "family")
+	for _, node := range exportNodes {
+		env := node.Env()
+		var value string
+		switch {
+		case env == nil:
+			value = ""
+
+		case env.EnvValue.Name != "":
+			if env.EnvValue.Name[0] == '$' {
+				env.EnvValue.Name = env.EnvValue.Name[1:]
+			}
+
+			value = e.systemTable[env.EnvValue.Name]
+
+		default:
+			value = env.EnvValue.Value
+
+		}
+		e.userTable[env.EnvVariable] = value
 	}
-
 	return nil
 }
 
 func (e *EnvAnalyzer) Reset() {
-	e.envTable = make(map[string]string)
+	e.userTable = make(map[string]string)
 }
 
 func (e *EnvAnalyzer) GetEnv() map[string]string {
-	return e.envTable
+	return e.userTable
 }
